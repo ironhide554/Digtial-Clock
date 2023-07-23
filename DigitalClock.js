ab8d7be5eb396cdb37b59b45dbad6ac0970952cd
Sparsh Tiwari <115800114+ironhide554@users.noreply.github.com>
2023-07-24 02:24:38 +0530

    Digital Clock Completed

--- DigitalClock.js
+++ DigitalClock.js
@@ -1,13 +1,37 @@
-let time = new Date()
- 
+
+
 
 
 
 let hours = document.getElementById("hours")
 let minutes = document.getElementById("minutes")
 let seconds = document.getElementById("seconds")
+let day = document.getElementById("day")
+let month = document.getElementById("month")
+let year = document.getElementById("year")
 
 
-hours.innerHTML() = time.getHours()
-minutes.innerHTML() = time.getMinutes()
-seconds.innerHTML() = time.getSeconds()
+
+setInterval(() => {
+    let time = new Date()
+
+    hours.innerHTML = (time.getHours() < 10 ? "0" : "") + time.getHours()
+    minutes.innerHTML = (time.getMinutes() < 10 ? "0" : "") + time.getMinutes()
+    seconds.innerHTML = (time.getSeconds() < 10 ? "0" : "") + time.getSeconds()
+
+
+}, 1000)
+
+
+let date = new Date()
+day.innerHTML = (date.getDate() < 10 ? "0" : "") + date.getDate()
+
+let mon =  date.getMonth()
+let mont = (Number.parseInt(mon))+1
+month.innerHTML = mont
+year.innerHTML = date.getFullYear()
+
+console.log(typeof mont)
+
+
+
