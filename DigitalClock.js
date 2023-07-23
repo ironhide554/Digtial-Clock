e380fd9bcf25263ab02030ea6cb4eb36717381dd
Sparsh Tiwari <115800114+ironhide554@users.noreply.github.com>
2023-07-23 22:52:21 +0530

    Basic Window

---
+++ DigitalClock.js
@@ -0,0 +1,13 @@
+let time = new Date()
+ 
+
+
+
+let hours = document.getElementById("hours")
+let minutes = document.getElementById("minutes")
+let seconds = document.getElementById("seconds")
+
+
+hours.innerHTML() = time.getHours()
+minutes.innerHTML() = time.getMinutes()
+seconds.innerHTML() = time.getSeconds()
